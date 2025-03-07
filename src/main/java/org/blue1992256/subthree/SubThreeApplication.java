c93b3a8e0f6bd8858d2fb0cc8984fb9a6e560ca3
blue1992256@mscbrain.com <blue1992256@mscbrain.com>
2025-03-07 11:33:30 +0900

    first commit

---
+++ src/main/java/org/blue1992256/subthree/SubThreeApplication.java
@@ -0,0 +1,13 @@
+package org.blue1992256.subthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubThreeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SubThreeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/blue1992256/subthree/SubThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blue1992256.subthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubThreeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
